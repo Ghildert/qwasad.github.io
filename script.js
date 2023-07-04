d96ebbe3ce29a9ae81a7b4e82ce6266fc9cd93d2
Ghildert <134858424+Ghildert@users.noreply.github.com>
2023-07-05 02:43:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function chanceSimulator(tries, chance) {
+    const tries = parseInt(document.getElementById("numberofTries").value);
+    const mins = parseInt(document.getElementById("minsStage").value)
+    const chance = parseFloat(document.getElementById("chancePercentage").value);
 
+    let stage = 0;
+    let outcome = [];
+  
+    for (let i = 0; i < tries; i++) {
+      if (Math.random() < chance) {
+        stage++;
+        outcome.push(i + 1);
+      }
+    }
+  
+    return {
+      stage: stage,
+      outcome: outcome
+    };
+
+    const resultElement = document.getElementById("result");
+    resultElement.innerText = `Your chance is: ${convChance}% \n Obtained in: ${result.outcome.join(", ")}. ---That's a total of ${result.stage} stage(s)`;
+}
