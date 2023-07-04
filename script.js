c3b73e284efabed3df514c5bb0a390d1b31f35e2
Ghildert <134858424+Ghildert@users.noreply.github.com>
2023-07-05 01:05:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-function runCounter() {
-  const numberofTries = parseInt(document.getElementById("numberofTries").value);
-  const chancePercentage = parseFloat(document.getElementById("chancePercentage").value);
-  
-  let count = 0;
 
-  function getRandomNumber() {
-    return Math.random() * numberofTries;
-  }
-
-  for (let i = 0; i < numberofTries; i++) {
-    if (getRandomNumber() <= chancePercentage) {
-      count++;
-    }
-  }
-
-  const resultElement = document.getElementById("result");
-  resultElement.innerText = "The " + chancePercentage + "% chance occurred " + count + " times.";
-}
